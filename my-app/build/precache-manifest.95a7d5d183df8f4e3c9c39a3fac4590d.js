4ade628cfea3ab502c9bc21fbcf68905a1864ae1
lakshmikorukanti <luckyrao4e8@gmail.com>
2020-09-17 21:46:11 +0530

    hackathon

---
+++ my-app/build/precache-manifest.95a7d5d183df8f4e3c9c39a3fac4590d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c537de60589c425dd3c62b7bbf82db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcc6b7c017b00d854c8f",
+    "url": "/static/css/main.a56a25a8.chunk.css"
+  },
+  {
+    "revision": "78b6532691c0a498713e",
+    "url": "/static/js/2.9148c49b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9148c49b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcc6b7c017b00d854c8f",
+    "url": "/static/js/main.58725e35.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
